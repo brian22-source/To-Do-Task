2ef808264ad5c38a786baa989b52b2dd07776db1
19carty <143806608+19carty@users.noreply.github.com>
2023-12-22 17:18:05 +0300

    first commit

---
+++ Script/auth.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBAay3HClYX2xE4V4pi87BLITPv3y9v30I",
+    authDomain: "to-do-task-5ef74.firebaseapp.com",
+    projectId: "to-do-task-5ef74",
+    storageBucket: "to-do-task-5ef74.appspot.com",
+    messagingSenderId: "633703865786",
+    appId: "1:633703865786:web:6679b264631f89959b5183",
+    measurementId: "G-768GE3MD2J"
+  };
